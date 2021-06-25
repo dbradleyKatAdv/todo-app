4e6cba6e9395467846ac869f6df3d89ab9b04eb3
Dustin Bradley <dbradley@katalystadvantage.com>
2021-06-25 07:30:50 -0400

    resolving delete functionality, working as expected

--- controllers/todo-controller.js
+++ controllers/todo-controller.js
@@ -48,7 +48,7 @@ getTodos = async (req, res) => {
 
 deleteTodo = async (req, res) => {
     const id = req.params.id;
-    await Todo.deleteOne({id:id}, (err) => {
+    await Todo.deleteOne(({_id: id}),   (err) => {
         if (err) {
             return res.status(400).json({ success: false, error: err })
         }
