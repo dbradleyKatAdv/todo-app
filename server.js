d5f1f95e88b1d4905d3569a6d7ee89e1095272eb
dustinbradley45 <dustinbradley45@live.com>
2021-07-13 19:13:24 -0400

    WIP commit

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const db = require('./database');
 const todoRouter = require('./routes/todo-router');
 const signupRouter = require('./routes/signup-router');
-// const loginRouter = require('./routes/login-router');
+const loginRouter = require('./routes/login-router');
 
 const app = express();
 const apiPort = 3001;
@@ -17,6 +17,6 @@ app.use(bodyParser.json());
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 app.use('/api/todos', todoRouter);
-// app.use('/api/users', signupRouter);
-// app.use('/api/login', signpRouter);
+app.use('/api/users', signupRouter);
+app.use('/api/login', loginRouter);
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`));
--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -7,7 +7,8 @@ function LoginPage() {
     const [loginPasswordInput, setLoginPasswordInput] = useState("");
 
     const handleLoginSubmit = (e) => {
-        console.log(e);
+        loginRequest();
+        e.preventDefault();
     }
 
     const handleLoginEmailChange = (inputValue) => {
@@ -21,6 +22,41 @@ function LoginPage() {
         setLoginPasswordInput(inputValue);
     }
 
+    const loginRequest = async () => {
+        try {
+            var headers = new Headers();
+            headers.append("Content-Type", "application/x-www-form-urlencoded");
+
+            var urlencoded = new URLSearchParams();
+            urlencoded.append("email", `${loginEmailInput}`);
+            urlencoded.append("password", `${loginPasswordInput}`);
+
+            var requestOptions = {
+                method: 'POST',
+                headers: headers,
+                body: urlencoded,
+                redirect: 'follow'
+            };
+
+            const response = await fetch("http://localhost:3001/api/login", requestOptions);
+            console.log(response)
+            // if (!response.ok) return setCreateUserSuccess(false);
+
+            const data = await response.json();
+            console.log(data)
+            if (data.success === true) {
+                // return setCreateUserSuccess(true);
+                console.log(data)
+            } else {
+                console.log(data)
+                // return setCreateUserSuccess(false)
+            }
+        } catch (err) {
+            // console.log(err)
+            // return setCreateUserSuccess(false);
+        }
+    }
+
     return (
         <Fragment>
             <LoginForm
@@ -30,7 +66,7 @@ function LoginPage() {
                 handleLoginEmailChange={handleLoginEmailChange}
                 handleLoginPasswordChange={handleLoginPasswordChange}
             />
-            <Link to="/">Signup</Link>
+            <Link to="/signup">Signup</Link>
         </Fragment>
        
     )
